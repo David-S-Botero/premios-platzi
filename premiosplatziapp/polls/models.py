e6fbd943bc83d952dc93b1c8ce394d11965af905
David Botero <davids.boteron@gmail.com>
2023-07-16 14:38:21 -0500

    App alpha upload with generic views and migrations, up to date

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Question(models.Model):
 
     question_text = models.CharField(max_length=200) 
-    pub_date = models.DateTimeField("Date published")
+    pub_date = models.DateTimeField("Date published", auto_now_add=True)
 
+    def __str__(self) -> str:
+        return self.question_text
+    
+    def was_publish_recently(self) -> bool :
+        return  self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    
 
 class Choice(models.Model):
 
@@ -13,4 +22,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __str__(self) -> str:
+        return self.choice_text
 
--- premiosplatziapp/premiosplatziapp/settings.py
+++ premiosplatziapp/premiosplatziapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9$q7lz50ft748)au1#23rx^a@8=k8i=n)r_h_w12o_r)@4)7+%'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 DJANGO_APPS = (
